a62e1d3bb5c3797e9297f4fcd728212baec8cb8b
Miszelze <pustola.michal@gmai.com>
2023-10-03 20:33:42 +0200

    test

--- Test.py
+++ Test.py
@@ -2,3 +2,8 @@ print ("gowno")
 x = 2+2
 
 print (x)
+
+## git init (tylko za pierwszym razem jak zakladasz repo)
+## git add .
+## git commit -m "Siema toi jest wiadomosc szczegolowo opisujaca ten commit"
+## git push
